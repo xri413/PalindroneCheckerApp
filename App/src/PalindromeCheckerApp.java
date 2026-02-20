c114978ee24de7f3fce43d82148facce3bb6bc65
xri413 <gaddamchakri1@gmail.com>
2026-02-20 09:33:27 +0530

    xri413

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+// version 1.0
+//author G Chakradhar
+// useCase 1; welcome page
+public class PalindromeCheckerApp { public static void main (String[] args){
+ System.out.println ("welcome to palindrome Checker App Management System");
+}
+}
