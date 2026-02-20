bb4eca6b7a71e10e4c881c254005ca2ecfd348a7
xri413 <gaddamchakri1@gmail.com>
2026-02-20 10:00:34 +0530

    xri413 Added UC1-WelcomePage

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -3,5 +3,7 @@
 // useCase 1; welcome page
 public class PalindromeCheckerApp { public static void main (String[] args){
  System.out.println ("welcome to palindrome Checker App Management System");
+ System.out.println("\nversion : 1.0");
+ System.out.println("\nSystem initialized successfully");
 }
 }
